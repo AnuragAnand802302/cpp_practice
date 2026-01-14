cc9be00075a9907c150040f686bd9444e3c20ef7
Anurag Anand <prafuljuly30@gmail.com>
2026-01-14 18:40:36 +0530

    first commit

---
+++ sum.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a=4, b=5;
+    int c = a + b;
+    cout << "Sum of " << a << " and " << b << " is: " << c << endl;
+    return 0;
+}
