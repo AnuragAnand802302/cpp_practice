281873079264651de30f18f9b69585634c269f34
Anurag Anand <prafuljuly30@gmail.com>
2026-01-24 21:08:48 +0530

    fht

---
+++ array_sort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int s;
+    cin>>s;
+    int a[s];
+    for(int i=0;i<s;i++){
+        cin>>a[i];
+    }
+    cout<<"unsorted array "<<endl;
+    for(int i=0;i<s;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl<<"sorted array in increasing order "<<endl;
+    sort(a,a+s);
+    for(int i=0;i<s;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl<<"sorted array in decreasing order "<<endl;
+    sort(a,a+s,greater<int>());
+    for(int i=0;i<s;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
+    
