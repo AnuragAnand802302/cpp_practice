e7f8a0028490aa19b0411e8ce6173b039020621f
Anurag Anand <prafuljuly30@gmail.com>
2026-01-31 22:11:07 +0530

    bhq

---
+++ array_reversed1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int s;
+    cin>>s;
+    int a[s];
+    for(int i=0;i<s;i++){
+        cin>>a[i];
+    }
+    cout<<"Original Array is "<<endl;
+    for(int i=0;i<s;i++){
+        cout<<a[i]<<" ";
+    }
+    for(int i=0;i<s/2;i++){
+        swap(a[i],a[s-i-1]);
+    }
+    cout<<endl<<"reversed array is "<<endl;
+    for(int i=0;i<s;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
